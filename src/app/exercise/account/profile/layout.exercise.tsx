d40a15311dde0288530e98eb0e9335ea2e10a365
Kevin <contact@kevinpaulidor.de>
2026-03-22 13:42:26 +0100

    fix: uncomment Tailwind styles and add title 'Zone Danger' in Layout component

--- src/app/exercise/account/profile/layout.exercise.tsx
+++ src/app/exercise/account/profile/layout.exercise.tsx
@@ -5,9 +5,10 @@ export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <div
     // 🤖 Décommente le style (tailwind) ci-dessous pour avoir une zone orange
-    // className="flex h-[200px] w-[400px] flex-col items-center justify-center border-4 border-yellow-700 bg-yellow-500 font-bold text-white"
+    className="flex h-[200px] w-[400px] flex-col items-center justify-center border-4 border-yellow-700 bg-yellow-500 font-bold text-white"
     >
       {/* 🐶 Ajoute le titre 'Zone Danger' */}
+      <h3>Zone Danger</h3>
       {children}
     </div>
   )
