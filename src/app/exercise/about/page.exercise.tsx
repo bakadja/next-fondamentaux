813cef9c6c8ec2abfc81ffaaaf577aa303ec42c3
Kevin <contact@kevinpaulidor.de>
2026-03-22 12:37:12 +0100

    uncomment console log for environment determination

--- src/app/exercise/about/page.exercise.tsx
+++ src/app/exercise/about/page.exercise.tsx
@@ -1,6 +1,7 @@
+//"use client"
 const Page = () => {
   // 🐶 Détermine dans quel environnement s'exécute ce composant grâce à un log
-  // 🤖 `console.log('Je suis un développeur FullStack Next')`
+  console.log("Je suis un développeur FullStack Next")
   //
   // 🐶 Vérifie dans la console du navigateur et dans la console du serveur (terminal)
 
